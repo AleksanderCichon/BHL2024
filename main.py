5ddf20cdd5953020234426b33dc2c1ab7a79019b
SeshiWai <seshiiiwaii@gmail.com>
2024-12-07 13:55:03 +0100

    added book page

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QStackedWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QStackedWidget, QLabel
+
+class TitleWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.book_button = QPushButton("Book flight")
+        self.exit_button = QPushButton("Exit")
+        #self.exit_button.clicked.connect(self.exit)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.book_button)
+        layout.addWidget(self.exit_button)
 
 
+class BookWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.label = QLabel("BOOK NOW")
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.label)
 
 class MainWindow(QWidget):
     def __init__(self, app):
@@ -12,23 +28,24 @@ class MainWindow(QWidget):
         self.setGeometry(100,100, 1000, 700)
 
         self.main_widgets = QStackedWidget()
-        self.title_widget = QWidget()
-        #self.showFullScreen()
-        #self.showFullScreen()
-        self.title_widget.book_button = QPushButton("Book flight")
-        self.title_widget.exit_button = QPushButton("Exit")
+        self.title_widget = TitleWindow()
+        self.book_widget = BookWindow()
+
         self.title_widget.exit_button.clicked.connect(self.exit)
-        layout = QVBoxLayout(self.title_widget)
-        layout.addWidget(self.title_widget.book_button)
-        layout.addWidget(self.title_widget.exit_button)
+        self.title_widget.book_button.clicked.connect(lambda: self.set_page(1))
         #self.exit_button.show()
 
         self.main_widgets.addWidget(self.title_widget)
+        self.main_widgets.addWidget(self.book_widget)
         main_layout = QVBoxLayout(self)
         main_layout.addWidget(self.main_widgets)
+
     
     def exit(self):
         app.exit()
+
+    def set_page(self, n):
+        self.main_widgets.setCurrentIndex(n)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
