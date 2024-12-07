d7e9f950ef740817f3355200798a7686f713102b
SeshiWai <seshiiiwaii@gmail.com>
2024-12-07 13:43:32 +0100

    added QStackedWidget as base

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QStackedWidget
 
 
-# Subclass QMainWindow to customize your application's main window
+
 class MainWindow(QWidget):
     def __init__(self, app):
         super().__init__()
         self.app = app
-        self.setWindowTitle("Flight Booker")
         self.setGeometry(100,100, 1000, 700)
+
+        self.main_widgets = QStackedWidget()
+        self.title_widget = QWidget()
         #self.showFullScreen()
         #self.showFullScreen()
-        self.book_button = QPushButton("Book flight")
-        self.exit_button = QPushButton("Exit")
-        self.exit_button.clicked.connect(self.exit)
-        layout = QVBoxLayout(self)
-        layout.addWidget(self.book_button)
-        layout.addWidget(self.exit_button)
+        self.title_widget.book_button = QPushButton("Book flight")
+        self.title_widget.exit_button = QPushButton("Exit")
+        self.title_widget.exit_button.clicked.connect(self.exit)
+        layout = QVBoxLayout(self.title_widget)
+        layout.addWidget(self.title_widget.book_button)
+        layout.addWidget(self.title_widget.exit_button)
         #self.exit_button.show()
+
+        self.main_widgets.addWidget(self.title_widget)
+        main_layout = QVBoxLayout(self)
+        main_layout.addWidget(self.main_widgets)
     
     def exit(self):
         app.exit()
