ed38483a624242d27300b7006366a4ce9b5c489a
SeshiWai <seshiiiwaii@gmail.com>
2024-12-07 13:20:50 +0100

    added 2 buttons

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
 
 # Subclass QMainWindow to customize your application's main window
-class MainWindow(QMainWindow):
-    def __init__(self):
+class MainWindow(QWidget):
+    def __init__(self, app):
         super().__init__()
-        self.setWindowTitle("My App")
-        self.setGeometry(100,100,200,200)
+        self.app = app
+        self.setWindowTitle("Flight Booker")
+        self.setGeometry(100,100, 1000, 700)
+        #self.showFullScreen()
+        #self.showFullScreen()
+        self.book_button = QPushButton("Book flight")
+        self.exit_button = QPushButton("Exit")
+        self.exit_button.clicked.connect(self.exit)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.book_button)
+        layout.addWidget(self.exit_button)
+        #self.exit_button.show()
+    
+    def exit(self):
+        app.exit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    window = MainWindow()
+    window = MainWindow(app)
     window.show()
 
     sys.exit(app.exec())
